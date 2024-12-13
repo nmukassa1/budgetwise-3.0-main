97aafedad625fa14ccbc9276ec7f1c337e9d3394
Nyah <nmukassa@gmail.com>
2024-12-13 22:53:52 +0000

    .

--- components/pots/potItemSummary/AddWithdrawButtons.tsx
+++ components/pots/potItemSummary/AddWithdrawButtons.tsx
@@ -3,7 +3,8 @@ import { PotType } from "@/lib/types";
 function AddWithdrawButtons({potItem, handleAddWithdrawMenuOpen}: { potItem: PotType, handleAddWithdrawMenuOpen:  (e: React.MouseEvent<HTMLButtonElement>) => void; }) {
     return (  <div className="flex items-center gap-4 mt-2">
         {potItem?.current_amount != null && potItem.current_amount > 0 && <button onClick={handleAddWithdrawMenuOpen} id="withdraw">- Withdraw</button>}
-        {(potItem?.current_amount === null || potItem?.current_amount < potItem?.target_amount) && (
+
+        {(potItem?.current_amount === null || potItem?.current_amount < potItem?.target_amount || potItem?.target_amount === 0) && (
   <button onClick={handleAddWithdrawMenuOpen} id="add">+ Add money</button>
 )}
     </div> );
